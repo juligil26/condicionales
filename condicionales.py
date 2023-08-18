990f8520445279f8cc0e7385970627dbad891c5b
julian <julis14189@gmail.com>
2023-08-18 10:26:50 -0500

    Nt3

---
+++ condicionales.py
@@ -0,0 +1,8 @@
+edadUsuario=int(input("Digia tu edad:"))
+
+if(edadUsuario>=18):
+    print("Bienvenido a la disco")
+else:
+    print("eres menor de edad, pa la casá")
+    print("oeee pa la casá...")
+
